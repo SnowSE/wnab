bf3c03988eb21284126398515ff260a5b20c8931
Omner <bookwormno2@gmail.com>
2025-09-24 13:15:59 -0600

    broken attempt to make maui app use api to show categories

--- src/WNAB.Maui/CategoriesPage.xaml.cs
+++ src/WNAB.Maui/CategoriesPage.xaml.cs
@@ -2,8 +2,22 @@ namespace WNAB.Maui;
 
 public partial class CategoriesPage : ContentPage
 {
-    public CategoriesPage()
+    private bool _loaded;
+
+    public CategoriesPage(CategoriesViewModel vm)
     {
         InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (_loaded) return;
+        _loaded = true;
+        if (BindingContext is CategoriesViewModel vm)
+        {
+            await vm.LoadAsync();
+        }
     }
 }
