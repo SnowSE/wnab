987014d318823c787cafa877be73f4dd26e478f2
aethagoan <aethagoan@outlook.com>
2025-10-31 11:45:21 -0600

    add migrations

--- src/WNAB.Data/Data/Migrations/20251031174501_transactionpageagain.Designer.cs
+++ src/WNAB.Data/Data/Migrations/20251031174501_transactionpageagain.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WNAB.Data;
 
 #nullable disable
 
-namespace WNAB.Data.Migrations
+namespace WNAB.Data.Data.Migrations
 {
     [DbContext(typeof(WnabContext))]
-    partial class WnabContextModelSnapshot : ModelSnapshot
+    [Migration("20251031174501_transactionpageagain")]
+    partial class transactionpageagain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WNAB.Data/Data/Migrations/20251031174501_transactionpageagain.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WNAB.Data.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class transactionpageagain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                table: "TransactionSplits",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "TransactionSplits",
+                newName: "Notes");
+        }
+    }
+}
