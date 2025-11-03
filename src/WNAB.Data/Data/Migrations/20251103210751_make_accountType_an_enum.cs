befacecfc749424e1c8a42daf2614acc12cddd50
Omner <bookwormno2@gmail.com>
2025-11-03 14:08:12 -0700

    migrations accountType is an enum

---
+++ src/WNAB.Data/Data/Migrations/20251103210751_make_accountType_an_enum.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WNAB.Data.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class make_accountType_an_enum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountType",
+                table: "Accounts",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountType",
+                table: "Accounts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
